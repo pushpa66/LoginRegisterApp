3b8165da987978c6bfc3da5ddf4a305ee5ad11d2
pushpa <pushpakumara.14@cse.mrt.ac.lk>
2016-12-19 23:02:09 +0530

    Login with server is completed

--- app/src/main/java/com/example/pushpa/loginregister/UserAreaActivity.java
+++ app/src/main/java/com/example/pushpa/loginregister/UserAreaActivity.java
@@ -1,5 +1,6 @@
 package com.example.pushpa.loginregister;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -12,8 +13,18 @@ public class UserAreaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area);
 
-        final EditText etUsername=(EditText)findViewById(R.id.etUsername);
-        final EditText etAge=(EditText)findViewById(R.id.etAge);
-        final TextView welcomeMessage=(TextView)findViewById(R.id.tvWelcomeMessage);
+        final EditText etUsername = (EditText)findViewById(R.id.etUsername);
+        final EditText etAge = (EditText)findViewById(R.id.etAge);
+        final TextView welcomeMessage = (TextView)findViewById(R.id.tvWelcomeMessage);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        String username = intent.getStringExtra("username");
+        int age = intent.getIntExtra("age",-1);
+
+        String message = name + " welcome to your user area";
+        welcomeMessage.setText(message);
+        etUsername.setText(username);
+        etAge.setText(age+"");
     }
 }
